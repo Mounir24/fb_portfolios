c619c796a9f421bca7aa6a88915d7c2af6ad956d
Mounir24 <M.brtouli997@gmail.com>
2022-06-19 17:40:34 +0200

    Remove Template section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Header from "../Components/Header"; // Header
 import Footer from "../Components/Footer";
-import temp_data from "../Components/temp_data";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Setup from "./Setup";
 import OAuth2Login from "react-simple-oauth2-login";
 import HomeBanner from "../images/home_banner.jpg";
@@ -95,37 +94,6 @@ function Home() {
           d="M0,160L48,181.3C96,203,192,245,288,229.3C384,213,480,139,576,122.7C672,107,768,149,864,186.7C960,224,1056,256,1152,240C1248,224,1344,160,1392,128L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
         ></path>
       </svg>
-      <div className="templates_section container-fluid">
-        <h1 className="section_title">Templates</h1>
-        <div className="container">
-          <div className="row row_defined_height w-75 mx-auto">
-            {temp_data.map((template, index) => {
-              return (
-                <div className="col-md-4" key={index}>
-                  <div className="card_temp_area">
-                    <span className="temp_img_wrapper">
-                      <img
-                        src={template.img_prev}
-                        alt="template 1"
-                        width="70px"
-                        height="70px"
-                      />
-                    </span>
-                    <div className="temp_details_area">
-                      <h3>{template.template_name}</h3>
-                    </div>
-                    <div className="wall">
-                      <div className="wall_wrapper">
-                        <Link to={template.preview_temp}>Preview</Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
       <Routes>
         <Route path="/setup" element={<Setup />} />
       </Routes>
